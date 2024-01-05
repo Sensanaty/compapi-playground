29b2796d7e59e885b64d0efaddf3f76f692d5ac0
Luka <salevic@lu-ka.me>
2024-01-05 15:27:20 +0100

    Make the VoipPanel an async component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="flex flex-col">
-    <VoipPanel v-if="!isVoipPanelOpen" />
+    <VoipPanel v-if="isVoipPanelOpen" />
     <button
       class="mx-auto mt-4 w-fit rounded-md bg-emerald-700 px-2 py-3 font-bold leading-5 text-black"
       @click.prevent="isVoipPanelOpen = !isVoipPanelOpen"
     >
-      {{ isVoipPanelOpen ? "Open" : "Close" }} Panel
+      {{ isVoipPanelOpen ? "Close" : "Open" }} Panel
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineAsyncComponent } from "vue";
 import useCallStore from "~/store/useCallStore";
-import VoipPanel from "~/components/VoipPanel/VoipPanel.vue";
 import { storeToRefs } from "pinia";
+
+const VoipPanel = defineAsyncComponent(() => import("~/components/VoipPanel/VoipPanel.vue"));
 
 const callStore = useCallStore();
 const { isVoipPanelOpen } = storeToRefs(callStore);
